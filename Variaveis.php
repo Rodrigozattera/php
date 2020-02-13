1bb478d1350c1e8a4cc3f3e397ca050821108e2c
Rodrigo Zattera <Thunderzats@gmail.com>
2020-02-13 20:17:58 -0200

    aula de variaveis

---
+++ Variaveis.php
@@ -0,0 +1,86 @@
+<?php
+
+/* aula 1
+
+ $nome = "Hcode";
+
+echo $nome;
+
+var_dump($nome);
+
+$anoNascimento = 2001;
+$nomeCompleto = "Rodrigo zattera";
+
+
+*/
+
+/*
+
+$nome1 = "rodrigo";
+$sobrenome = "zattera";
+
+
+$nomeCompleto = $nome1." ".$sobrenome; // o . concatena
+
+
+echo $nomeCompleto;
+
+exit;
+
+echo $nome1;
+
+echo "<br/>";
+
+unset($nome1); //tirar a variavel da memoria
+
+if(isset($nome1)){ // isset = se ela exite
+
+	echo $nome1;
+}
+
+*/
+
+//tipos de dados
+
+//basicos
+/*
+
+$nome = "Hcode"; //string
+
+$site = 'www.hcode.com.br'; //
+
+$ano = 1990; //inteiro
+
+$salario = 5500.99; //float
+
+$bloqueado = false; //boleano
+
+// compostos
+
+$frutas = array("abacaxi", "laranja","manga"); //vetor/array
+
+//echo $frutas[2];
+
+$nascimento = new DateTime(); //objeto
+
+//var_dump($nascimento);
+
+//tipos especiais
+
+$arquivo = fopen("variavel.php", "r");
+
+var_dump($arquivo);
+
+$nulo = NULL; //ela n existe NULL
+
+$vazio = "";// existe e esta esperando alguma informacao.
+
+*/
+
+
+
+
+
+
+?>
+
