92b5054be0a8b2e18aec1eba093bfa308a785d17
Rodrigo Zattera <Thunderzats@gmail.com>
2020-02-13 21:36:10 -0200

    add os operadores.php

---
+++ operadores.php
@@ -0,0 +1,123 @@
+<?php  
+// operadores
+/*
+$nome="Hcode";
+
+echo $nome . "mais alguma coisa</br>"; // . add
+
+$nome .= "treinamentos";// .= pega a string e adiciona algo a ele.
+
+echo $nome;
+
+*/
+
+/*
+$valorTotal = 0;
+
+$valorTotal += 100;// += pega ele mesmo e soma com o valor digitado
+
+$valorTotal += 25;
+
+$valorTotal *= .9;// ele x 0.90;
+echo $valorTotal;
+
+*/
+
+/*
+$a = 10;
+$b = 2;
+
+echo $a+$b;
+
+echo "<br>";
+
+echo $a-$b;
+
+echo "<br>";
+
+echo $a*$b;
+
+echo "<br>";
+
+echo $a/$b;
+
+echo "<br>";
+
+echo $a%$b;
+
+echo "<br>";
+
+echo $a ** $b; // 10 elevado a 2
+*/
+
+/*
+$a=55.0;
+$b=55;
+
+var_dump($a > $b);
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+var_dump($a == $b);
+
+echo "<br>";
+
+var_dump($a === $b);// saber se exatamente tudo e igual
+
+
+echo "<br>";
+
+var_dump($a != $b);
+
+echo "<br>";
+
+var_dump($a !== $b);
+*/
+/*
+$a = 35;
+$b = 60;
+
+var_dump($a <=> $b);// 1 retorna 1 se for maior se for igual 0 se o da direita for maior retorna -1
+*/
+/*
+$a = NULL;
+
+$b = 8;
+
+$c = 10;
+
+echo $a ?? $b ?? $c; //se o 1 for null mostra o b se o b for null mostra o c 
+*/
+/*
+$a = 10;
+
+echo $a++;// soma mais 1 ao valor depois de exibilo
+
+echo "<br>";
+
+echo $a;
+
+echo "<br>";
+
+echo ++$a;
+
+echo "<br>";
+
+echo --$a;
+*/
+/*
+$resultado = (10 + 3) / 2 > 5 && 10 + 5 < 20; // matematica base de prioridad
+
+$resultado = (10 + 3) / 2 > 5 || 10 + 5 < 20; // || ou;
+
+//echo $resultado;
+
+var_dump($resultado);
+*/
+
+
+?>
