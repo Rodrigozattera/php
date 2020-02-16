042482503327d4a08a04ecdabc28d8bc3d1b2017
Rodrigo Zattera <Thunderzats@gmail.com>
2020-02-16 16:28:10 -0300

    att functions

--- function.php
+++ function.php
@@ -140,23 +140,137 @@ echo "<br>";
 
 */
 
+/*
+
+$hierarquia = array(
+
+	array(
+
+		'nome_cargo' => 'CEO',
+		'subordinados'=> array(
+			// inicio: Diretor
+			array(
+
+				'nome_cargo'=>'Diretor Comercial',
+				'subordinados'=>array(
+					//inicio: Gerente de Vendas
+					array(
+						'nome_cargo'=>'Gerente de Vendas'
+					)
+					//termino: Gerente de Vendas
+
+				)
+			),
+
+			//termino: Diretor Comercial
+			//inicio : Diretor Financeiro
+			array(
+				'nome_cargo'=>'Diretor Financeiro',
+				'subordinados'=>array(
+					//inicio: gerente de contas a pagar
+					array(
+						'nome_cargo'=>'Gerente de Contas a Pagar',
+						'subordinados'=>array(
+							//inicio: Supervisor de pagamentos
+							array(
+								'nome_cargo'=>'Supervidor de Pagamentos'
+							)
+
+							//termino: Supervisor de pagamentos
+						)
+					),
+					//termino: gerente de contas a pagar
+					//inicio: Gerente de compras
+					array(
+						'nome_cargo'=>'Gerente de compras',
+						'subordinados'=>array(
+							//inicio:Supervisor de suprimentos
+							array(
+								'nome_cargo'=>'Supervidor de Suprimentos'
+							)
+							//Termino: Supervisor de suprimentos
+						)
+					)
+					//termino:gerente de compras
+				)
+
+			)
+			//termino: Diretor Financeiro
+
+		)
+
+	)
+
+);
+
+function exibe($cargos){
+
+	$html = '<ul>';
+
+	foreach ($cargos as $cargo) {
+
+		$html .="<li>";
+
+		$html .=$cargo['nome_cargo'];
+
+		if(isset($cargo['subordinados'])&& count($cargo['subordinados'])> 0){
+
+			$html .= exibe($cargo['subordinados']);
+
+
+		}
+
+		$html .="</li>";
+
+
+	}
+
+
+	$html .="</ul>";
+
+	return $html;
+
+
+};
+
+echo exibe($hierarquia);
+
+
+*/
+
+/*
+function teste($callback){
+
+
+	//processo lento
+
+	$callback();
+
+
+
+}
+
+
+teste(function(){
+
+	echo "terminou";
+
+
+});
 
 
 
 
+$fn = function($a){
 
 
+	var_dump($a);
 
+};
 
+$fn("ola");
 
-
-
-
-
-
-
-
-
+*/
 
 
 
